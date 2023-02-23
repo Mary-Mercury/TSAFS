10b6f9c992896dbe908114da42559e4031e192ee
MercuryX <virusgame1990@gmail.com>
2023-02-23 15:35:41 +0300

    create and designed second activity

---
+++ app/src/main/java/com/example/tablesync/TableActivity.kt
@@ -0,0 +1,14 @@
+package com.example.tablesync
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TableActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.table)
+        supportActionBar?.hide()
+
+
+    }
+}
